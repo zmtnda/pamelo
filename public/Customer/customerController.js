8acec8681da51881f97602f374a14bb3445559c3
sbilich <sbilich@calpoly.edu>
2016-10-30 12:38:49 -0700

    Update buttons and views on User pages

--- public/Customer/customerController.js
+++ public/Customer/customerController.js
@@ -1,5 +1,12 @@
 app.controller('customerController', ['$scope', '$state','registerPopService', '$http', 'notifyDlg', '$rootScope', 'logService',function(scope, state, regPopSer, http, noDlg, rscope, logSer) {
     scope.user = {};
+    scope.viewEnum = {
+      NONE: 0,
+      PERSONAL: 1,
+      SERVICES: 2,
+      HISTORY: 3
+    };
+
     scope.isShowPersonalField = 0;
     scope.isShowListServices = 0;
     scope.isShowOrderHistory = 0;
@@ -7,21 +14,44 @@ app.controller('customerController', ['$scope', '$state','registerPopService', '
     scope.field = {};
     scope.hists = [];
 
-    scope.showListServices = function(){scope.isShowListServices = 1;}
-    scope.ShowOrderHistory = function(){scope.isShowOrderHistory = 1;}
+    scope.switchView = function(newView) {
+      switch (newView) {
+        case scope.viewEnum.NONE:
+          scope.isShowPersonalField = 0;
+          scope.isShowListServices = 0;
+          scope.isShowOrderHistory = 0;
+          break;
+        case scope.viewEnum.PERSONAL:
+          scope.isShowPersonalField = 1;
+          scope.isShowListServices = 0;
+          scope.isShowOrderHistory = 0;
+          break;
+        case scope.viewEnum.SERVICES:
+          scope.isShowListServices = 1;
+          scope.isShowPersonalField = 0;
+          scope.isShowOrderHistory = 0;
+          break;
+        case scope.viewEnum.HISTORY:
+          scope.isShowOrderHistory = 1;
+          scope.isShowListServices = 0;
+          scope.isShowPersonalField = 0;
+          break;
+      }
+    }
+
     scope.modifyCustomer = function(){
       if (scope.isShowPersonalField == 0) {
-        scope.isShowPersonalField = 1;
+        scope.switchView(scope.viewEnum.PERSONAL);
       } else {
-        scope.isShowPersonalField = 0;
+        scope.switchView(scope.viewEnum.NONE);
       }
     }
 
     scope.showOrderHistory = function(){
         if (scope.isShowOrderHistory == 1) {
-          scope.isShowOrderHistory = 0;
+          scope.switchView(scope.viewEnum.NONE);
         } else {
-          scope.isShowOrderHistory = 1;
+          scope.switchView(scope.viewEnum.HISTORY);
           http.get('User/serviceHistory/all')
           .then(function(res){
                 scope.hists = res.data;
@@ -32,7 +62,7 @@ app.controller('customerController', ['$scope', '$state','registerPopService', '
 
     scope.postModify = function(){
         //modify!!
-        scope.isShowPersonalField = 0;
+        scope.switchView(scope.viewEnum.NONE);
         http.put("User/"+rscope.loggedUser.id, scope.user)
         .then(function(){
           scope.user = {};
@@ -62,9 +92,9 @@ app.controller('customerController', ['$scope', '$state','registerPopService', '
 
     scope.showListServices = function(){
       if (scope.isShowListServices == 1) {
-        scope.isShowListServices = 0;
+        scope.switchView(scope.viewEnum.NONE);
       } else {
-        scope.isShowListServices = 1;
+        scope.switchView(scope.viewEnum.SERVICES);
         http.get('Serv/')
         .then(function(response) {
            scope.listServices = response.data;
