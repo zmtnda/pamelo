35c0ec570729ee0b0d9b899f6c7535585ca84789
Aaron Otis <otis@openmailbox.org>
2016-11-23 14:56:26 -0800

    Added password hashing via Bcrypt and needed checks for logging in. Updated sql scripts to include hashed passwords instead of plaintext ones.

--- Database/createAdmin.sql
+++ Database/createAdmin.sql
@@ -1,4 +1,4 @@
 INSERT INTO Users
 (id, email, password, role, firstName, lastName, phone, whenRegistered)
-VALUES (1, "Admin@11.com", "password", 2, "Admin", "IAM", 123456789, NOW());
+VALUES (1, "Admin@11.com", "$2a$10$rvRGZjzxZnw5JFjz65nye./4W/PcdJjtH3izStW.im8xknJzAINJ.", 2, "Admin", "IAM", 123456789, NOW());
 
